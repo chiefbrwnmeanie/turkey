a39446826791e498c5f6bd15efc6defc623ebfca
chiefbrwnmeanie <liz@elizabethsanchez.com>
2017-11-30 21:09:33 -0700

    refactored app module for clean-up. added app material module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProfileComponent} from './profile/profile.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
