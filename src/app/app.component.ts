44854aa13a7bedc4f982204fd634b31740090f36
chiefbrwnmeanie <liz@elizabethsanchez.com>
2017-12-01 22:02:02 -0700

    started responsive sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ViewChild, ChangeDetectorRef, Component, OnInit  } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import {MatSidenavContainer} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Turkey';
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  @ViewChild('snav') public myNav: MatSidenavContainer;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  onResize() {
+    this.manageNav();
+  }
+
+  ngOnInit() {
+    this.manageNav();
+  }
+
+  manageNav(): void {
+    this.mobileQuery.matches ? this.myNav.close() : this.myNav.open();
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
 }
